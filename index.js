d77a462e2ca6dece61dffd8d651891d024c246a9
Bartek <bartekma89@gmail.com>
2018-01-22 00:50:48 +0100

    done

---
+++ index.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var Color = require('colors');
+
+fs.readdir(__dirname, function(err, files) {
+    if(err) throw err.message;
+    
+    files.forEach(function(file) {
+        console.log(file);
+    })
+    
+    fs.writeFile('./path.txt', files, function(err) {
+        if(err) throw err.message;
+        
+        console.log('Dane zapisane do pliku'.green);
+    })
+})
+
+fs.readFile('./path.txt', function(err, data){
+    if(err) throw err.message;
+    
+    console.log(data.toString());
+    console.log('Dane odczytane z pliky'.blue);
+})
