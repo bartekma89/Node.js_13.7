4e3f103ced3d5535550f1ab5b93cd7503ac9478f
Bartek <bartekma89@gmail.com>
2018-01-23 12:27:30 +0100

    refactored code

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 var fs = require('fs');
 var Color = require('colors');
 
-var writeStream = fs.createWriteStream('./path.txt');
-var readStream = fs.createReadStream('./path.txt');
+var fileToStoreData = 'path.txt';
 
-fs.readdir(__dirname, function (err, files) {
+var writeContent = function (fileData) {
+    fs.readdir(__dirname, function (err, files) {
 
-    if (err) throw err.message;
+        if (err) throw err.message;
 
-    files.forEach(function (file) {
-        console.log(file);
+        files.forEach(function (file) {
+            console.log(file);
+        })
+
+        var writeStream = fs.createWriteStream(fileData, {
+            encoding: 'utf8'
+        });
+        writeStream.write(files.toString());
+        writeStream.end();
+        writeStream.on('finish', function () {
+            console.log('Save data completed'.green);
+            readContent(fileData);
+        })
+
     })
+}
 
-    writeStream.write(files.toString());
-    writeStream.end();
-    writeStream.on('finish', function () {
-        console.log('Save data completed'.green);
+var readContent = function (fileData) {
+    fs.createReadStream(fileData).on('data', function (chunk) {
+        console.log('Data read from file'.blue);
+        console.log(chunk.toString());
     })
+}
 
-})
-
-readStream.on('data', function (chunk) {
-    console.log(chunk.toString());
-    console.log('Data read from file'.blue);
-})
-
-readStream.pipe(process.stdout);
+writeContent(fileToStoreData);
